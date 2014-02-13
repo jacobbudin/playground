6e9492cf2f617a874e7930cd14bb7dc78c1b4d1e
Jacob Budin <self@jacobbudin.com>
2014-02-12 22:29:09 -0500

    Integrating Boris as part of Playground, adding command line options

---
+++ src/Playground/CLIOptionsHandler.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Playground;
+
+/**
+ * Processes available command line flags.
+ */
+class CLIOptionsHandler {
+	/**
+	 * Accept the REPL object and perform any setup necessary from the CLI flags.
+	 *
+	 * @param Playground $playground
+	 */
+	public function handle($playground) {
+		$args = getopt('hv', array('help', 'version'));
+
+		foreach ($args as $option => $value) {
+			switch ($option) {
+				/*
+				 * Show Usage info
+				 */
+				case 'h':
+				case 'help':
+					$this->_handleUsageInfo();
+				break;
+
+				/*
+				 * Show version
+				 */
+				case 'v':
+				case 'version':
+					$this->_handleVersion();
+				break;
+
+				default:
+					$this->_handleRequire($playground, array_slice($argv, 2));
+				break;
+			}
+		}
+	}
+
+	// -- Private Methods
+
+	private function _handleRequire($playground, $names) {
+		var_dump($names);
+		/*
+		$require = array_reduce(
+			(array) $paths,
+			function($acc, $v) { return array_merge($acc, explode(',', $v)); },
+			array()
+		);
+
+		$boris->onStart(function($worker, $scope) use($require) {
+			foreach($require as $path) {
+				require $path;
+			}
+
+			$worker->setLocal(get_defined_vars());
+		});
+		 */
+	}
+
+	private function _handleUsageInfo() {
+		echo <<<USAGE
+Usage: playground [options]
+playground is a tool for experimenting with Composer vendor libraries via the Boris REPL
+
+Options:
+	-h, --help      show this help message and exit
+	-v, --version   show Playground version
+
+USAGE;
+		exit(0);
+	}
+
+	private function _handleVersion() {
+		printf("Playground %s\n", Playground::VERSION);
+		exit(0);
+	}
+}
+
--- src/Playground/Playground.php
+++ src/Playground/Playground.php
@@ -3,7 +3,34 @@
 namespace Playground;
 
 class Playground{
+	const VERSION = '1.0.0';
+
+	private $_packages = array();
+	private $_boris;
+
+	/**
+	 * Create a new Playground environment
+	 */
 	public function __construct(){
-		echo 'Welcome to Playground', PHP_EOL;
+		$this->_boris = new \Boris\Boris();
+	}
+
+	/**
+	 * Set Composer packages to be installed (if necessary) and referenced
+	 *
+	 * @param array $packages Composer-style paths
+	 */
+	public function setPackages($packages) {
+		$this->_packages = $packages;
+	}
+
+	/**
+	 * Start Boris
+	 *
+	 * This method never returns.
+	 */
+
+	public function start() {
+		$this->_boris->start();
 	}
 }
